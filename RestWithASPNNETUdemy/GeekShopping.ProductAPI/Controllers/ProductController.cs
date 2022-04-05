ff6cc6248dcc485800c8cb63671acd5cff81f2ec
Silvio Oliveira <silvio.oliveira@arena22.com.br>
2022-04-05 14:31:01 -0300

    Configuracao inicial do controller

---
+++ RestWithASPNNETUdemy/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FindById(int id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if (productVO == null)
+            {
+                return BadRequest();
+            }
+
+            var product = await _repository.Create(productVO);
+            return Ok(product);
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if (productVO == null)
+            {
+                return BadRequest();
+            }
+
+            var product = await _repository.Update(productVO);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
--- RestWithASPNNETUdemy/GeekShopping.ProductAPI/Program.cs
+++ RestWithASPNNETUdemy/GeekShopping.ProductAPI/Program.cs
@@ -24,10 +24,7 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen( c =>
-{
-    c.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "GeekShopping.ProductAPI" });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
