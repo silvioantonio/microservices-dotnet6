f6cdbbbbe5582f5de8b250bd37cebc1f7a3eb1f8
Silvio Oliveira <silvio.oliveira@arena22.com.br>
2022-04-05 16:33:21 -0300

    Configurado modelo e serviçoes do front

---
+++ RestWithASPNNETUdemy/GeekShopping.web/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekShopping.web.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ RestWithASPNNETUdemy/GeekShopping.web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.web.Models;
+
+namespace GeekShopping.web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById();
+        Task<ProductModel> CreateProduct(ProductModel product);
+        Task<ProductModel> UpdateProduct(ProductModel product);
+        Task<bool> DeleteProductById(int id);
+    }
+}
