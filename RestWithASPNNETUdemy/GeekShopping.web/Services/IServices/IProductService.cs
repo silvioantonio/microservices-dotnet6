9873b6d14e96b8aab261959ce7b21ffdc76dae3f
Silvio Oliveira <silvio.oliveira@arena22.com.br>
2022-04-05 16:41:58 -0300

    implementando services no front

--- RestWithASPNNETUdemy/GeekShopping.web/Services/IServices/IProductService.cs
+++ RestWithASPNNETUdemy/GeekShopping.web/Services/IServices/IProductService.cs
@@ -5,7 +5,7 @@ namespace GeekShopping.web.Services.IServices
     public interface IProductService
     {
         Task<IEnumerable<ProductModel>> FindAllProducts();
-        Task<ProductModel> FindProductById();
+        Task<ProductModel> FindProductById(int id);
         Task<ProductModel> CreateProduct(ProductModel product);
         Task<ProductModel> UpdateProduct(ProductModel product);
         Task<bool> DeleteProductById(int id);
---
+++ RestWithASPNNETUdemy/GeekShopping.web/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using GeekShopping.web.Models;
+using GeekShopping.web.Services.IServices;
+using GeekShopping.web.Utils;
+
+namespace GeekShopping.web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+
+        public const string BasePath = "api/v1/product";
+
+        public Task<ProductModel> CreateProduct(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(int id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public Task<ProductModel> UpdateProduct(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
