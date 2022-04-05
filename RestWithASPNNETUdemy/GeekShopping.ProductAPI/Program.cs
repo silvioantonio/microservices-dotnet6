8ad1f9d3b70f6a35f1b5b9bf4dafe2083b7979be
Silvio Oliveira <silvio.oliveira@arena22.com.br>
2022-04-05 13:40:38 -0300

    Adicionando padrao VO ao projeto e mapping entre classe VO e modelo

--- RestWithASPNNETUdemy/GeekShopping.ProductAPI/Program.cs
+++ RestWithASPNNETUdemy/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 28))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
