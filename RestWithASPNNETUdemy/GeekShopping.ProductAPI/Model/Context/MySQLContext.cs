278d63981a3bb95784b86e5f1aed26d2e7c9b887
Silvio Oliveira <silvio.oliveira@arena22.com.br>
2022-04-05 15:00:36 -0300

    Seeds banco

--- RestWithASPNNETUdemy/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ RestWithASPNNETUdemy/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -9,5 +9,99 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 7,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 8,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 9,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 10,
+                Name = "Notebook 14'",
+                CategoryName = "Eletronicos",
+                Description = "Notebook ideapad S145 com melhor custo benêficios",
+                Price = new decimal(6500.59),
+                ImageUrl = "//"
+            });
+        }
     }
 }
